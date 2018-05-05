56fc4621d62b4e494be989506f6960be2ce9aed9
Rafkat Galiullin <ufadiz@gmail.com>
2018-05-05 16:10:16 +0500

    add bulma css framework

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
+import Buefy from "buefy";
+import "buefy/lib/buefy.css";
 import App from "./App.vue";
 import router from "./router";
 
+Vue.use(Buefy);
 Vue.config.productionTip = false;
 
 new Vue({
